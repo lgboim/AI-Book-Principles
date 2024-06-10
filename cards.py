7d0d9724b5b29663928cdb12233e968b2fc1aa65
Arielboim <lgboim@gmail.com>
2024-06-10 22:42:36 +0300

    Update cards.py

--- cards.py
+++ cards.py
@@ -4,10 +4,8 @@ import os
 import uuid
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 # Database configuration
 DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///local_database.db')
